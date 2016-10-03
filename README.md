a876050870f04901a49da3d1e2320cabebba3538
LightIT <an.pavlovych@gmail.com>
2016-10-03 17:57:02 +0300

    add js for spiders management page

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ Run spiders:
         - scrapy crawl midlandici
     easyroommate
         - scrapy crawl easyroommate -a search_address='california 1'
+
+Deploy scrapyd
+    - pip install scrapyd
+    - pip install scrapyd-client
+    - scrapyd-deploy -L scrapyd
+    - scrapyd-deploy scrapyd -p crawlestate
+    - add to scrapy.cfg
+        [deploy:scrapyd]
+        url = http://localhost:6800/
+        project = crawlestate
+    - add scrapyd.conf
+        [scrapyd]
+        eggs_dir    = eggs
+        logs_dir    = logs
+        items_dir   = items
+        jobs_to_keep = 5
+        dbs_dir     = dbs
+        max_proc    = 0
+        max_proc_per_cpu = 4
+        finished_to_keep = 100
+        poll_interval = 5
+        http_port   = 6800
+        debug       = off
+        runner      = scrapyd.runner
+        application = scrapyd.app.application
+        launcher    = scrapyd.launcher.Launcher
--- static/js/default.js
+++ static/js/default.js
@@ -1,10 +1,50 @@
+// $(document).ready(function(){
+//     $('.messages').find('.alert').each(function(){
+//         var el = $(this);
+//         setTimeout(function(){
+//             el.fadeOut(500, function(){
+//                el.remove();
+//             });
+//         }, 4000);
+//     });
+// });
+var base_scrapyd_url = 'http://localhost:6800/';
+
 $(document).ready(function(){
-    $('.messages').find('.alert').each(function(){
-        var el = $(this);
-        setTimeout(function(){
-            el.fadeOut(500, function(){
-               el.remove();
-            });
-        }, 4000);
+    $.get(base_scrapyd_url + "listjobs.json?project=crawlestate", function(data){
+        console.log("Data: " + data);
+        var html = '';
+        var status = ['running', 'finished', 'pending'];
+        for (var i=0; i < status.length; i++) {
+            for(var j=0; j < data[status[i]].length; j++) {
+                var btn = '';
+                if (status[i] == 'pending' || status[i] == 'running'){
+                    btn = '<a href="' + data[status[i]][j]['id'] + '" class="cancel btn btn-primary">Cancel</a>';
+                }
+                html += '<tr id="' + data[status[i]][j]['id'] + '">\
+                        <td>' + data[status[i]][j]['id'] + '</td>\
+                        <td>' + data[status[i]][j]['spider'] + '</td>\
+                        <td>' + data[status[i]][j]['start_time'] + '</td>\
+                        <td>' + data[status[i]][j]['end_time'] + '</td>\
+                        <td>' + status[i] + '</td>\
+                        <td>' + btn + '</td>\
+                    </tr>';
+            }
+        }
+        $('table tr').after(html);
     });
+    
+    $(document).on('click', '.cancel', function(e){
+        console.log('TRY');
+        e.preventDefault();
+        var job_id = $(this).attr('href');
+        var data = {
+            'project': 'crawlestate',
+            'job': job_id
+        }
+        $.post(base_scrapyd_url + "cancel.json", data, function(data) {
+          console.log( data );
+        });
+    })
+    
 });

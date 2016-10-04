5ae6d97d40a764bd12209ad250ef8f63f0242c71
LightIT <an.pavlovych@gmail.com>
2016-10-04 17:24:38 +0300

    change settings

--- static/js/default.js
+++ static/js/default.js
@@ -8,7 +8,7 @@
 //         }, 4000);
 //     });
 // });
-var base_scrapyd_url = 'http://172.16.205.1:6800/';
+var base_scrapyd_url = 'http://localhost:6800/';
 
 $(document).ready(function(){
     var update_spiders_table = function(){
